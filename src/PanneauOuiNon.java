fa8a06936c2f5da9727b854169de53b76b5a00d6
antoshig13 <anthochig533@gmail.com>
2017-04-13 16:50:19 -0400

    Panneauouinon avec un main pour faire des tests

--- src/PanneauOuiNon.java
+++ src/PanneauOuiNon.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,10 +16,10 @@ import javax.swing.*;
 
 public class PanneauOuiNon extends JPanel{
 
+	private int index;
 	private BdQuestionsReponses bd;
     private PanneauPrincipal principal;
-    private JButton buttonOui = new JButton();
-    private JButton buttonNon = new JButton();
+    
 
     public PanneauOuiNon(BdQuestionsReponses bd, PanneauPrincipal principal){
         this.bd = bd;
@@ -54,11 +56,47 @@ public class PanneauOuiNon extends JPanel{
 	
 	public void add(Container pane, int index){
 		
-		
+		pane.add(pane, index);
 		
 		
 	}
 	
+	public static void addComponents() {
+		
+
+        
+    }
+ 
+	private void addPanneau(){
+
+       
+	}
+   
+    
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("BoxLayoutDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+       
+        JPanel panOuiNon = new JPanel(); 
+		
+		JButton buttonOui = new JButton("OUI");
+	    JButton buttonNon = new JButton("NON");
+	    
+	    
+        
+	   
+        panOuiNon.setAlignmentX(CENTER_ALIGNMENT);
+        panOuiNon.add(buttonOui);
+        panOuiNon.add(buttonNon);
+        frame.setSize(600, 600);
+        frame.add(panOuiNon,BorderLayout.SOUTH);
+        
+        
+        //Display the window.
+        frame.setVisible(true);
+    }
 	
 	private class MyActionListenerOui implements ActionListener{
 
@@ -77,7 +115,15 @@ public class PanneauOuiNon extends JPanel{
 
     }
 	
-	
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 	
 
     
